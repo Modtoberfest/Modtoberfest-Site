3c8e815c57d2f29b113c78507cfa7d905ad9355c
Antoine Gagnon <antoine.gagnon760@gmail.com>
2020-09-12 16:36:26 -0400

    Add next-auth

--- components/NavUser.js
+++ components/NavUser.js
@@ -1,20 +1,53 @@
 import React from "react";
 import Link from "next/link";
+import classNames from "classnames";
 
-export default function NavUser({ profile }) {
+import { signin, signout, useSession } from "next-auth/client";
+
+export default function NavUser() {
+  const [session, loading] = useSession();
+
   const classes =
-    "block mt-4 lg:inline-block lg:mt-0 text-white font-bold bg-accent-red hover:bg-accent-red-700 px-4 py-2 rounded-full";
-  if (profile) {
+    "mt-4 lg:mt-0 text-white font-bold bg-accent-red hover:bg-accent-red-700 px-4 py-2 rounded-full";
+
+  if (session) {
     return (
-      <Link href="/progress">
-        <a className={classes}>My progress</a>
-      </Link>
+      <div className="flex items-center">
+        <a
+          href={`/api/auth/signout`}
+          onClick={(e) => {
+            e.preventDefault();
+            signout();
+          }}
+        >
+          <button className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            Sign out
+          </button>
+        </a>
+        <Link href="/progress">
+          <a className={classNames(classes, "flex items-center")}>
+            <img
+              src={session.user.image}
+              width="40"
+              className="rounded-full mr-2"
+            />
+            <div>My progress</div>
+          </a>
+        </Link>
+      </div>
     );
   } else {
     return (
-      <Link href="/login">
-        <a className={classes}>Login</a>
-      </Link>
+      <a
+        href={`/api/auth/signin`}
+        onClick={(e) => {
+          e.preventDefault();
+          signin();
+        }}
+        className={classes}
+      >
+        Login
+      </a>
     );
   }
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import "../css/index.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { Provider } from "next-auth/client";
+
+function App({ Component, pageProps }) {
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp;
+export default App;
