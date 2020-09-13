d0fb3831158d452032027e30dfc833215109283a
Antoine Gagnon <antoine.gagnon760@gmail.com>
2020-09-13 13:41:08 -0400

    Remove TypeORM

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,7 +1,15 @@
 import sequelize from "../../lib/database";
+// const Participant = require("../../models/Participant");
+import Participant from "../../models/Participant";
 
 export default async (req, res) => {
   res.statusCode = 200;
+  console.log(Participant);
+  const jane = await Participant.create({
+    firstName: "Jane",
+    lastName: "Doe",
+  });
+
   res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ name: "John Doe" }));
+  res.end(JSON.stringify({ jane }));
 };
