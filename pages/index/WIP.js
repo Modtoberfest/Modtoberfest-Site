2d660cec8f0347e74f1749fa66282f5c0760c404
Antoine Gagnon <antoine.gagnon760@gmail.com>
2020-09-14 22:47:27 -0400

    fine tune wip page

--- pages/index/WIP.js
+++ pages/index/WIP.js
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function WIP() {
   return (
-    <div className="bg-gray-900 text-white flex items-center min-h-screen justify-center">
-      <h1>Work in progress!</h1>
+    <div className="bg-gray-900 text-white flex flex-col items-center min-h-screen justify-center">
+      <h1>Modtoberfest 2020</h1>
+      <p>Work in progress! Stay sturned :)</p>
     </div>
   );
 }
