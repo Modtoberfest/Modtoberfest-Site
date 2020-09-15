c005a6f7e152c66ddec870e6fa06b4a195223169
Antoine Gagnon <antoine.gagnon760@gmail.com>
2020-09-14 22:44:38 -0400

    Add WIP page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import Layout from "../components/Layout";
+import { getEventStage } from "../lib/stage";
 import Details from "./index/Details";
 import MainSection from "./index/MainSection";
+import WIP from "./index/WIP";
 
 function IndexPage() {
+  const stage = getEventStage();
+
+  if (stage == "pre" && new Date().getUTCDay() + 1 < 21) {
+    return <WIP />;
+  }
+
   return (
     <Layout>
       <MainSection />
---
+++ pages/index/WIP.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function WIP() {
+  return (
+    <div className="bg-gray-900 text-white flex items-center min-h-screen justify-center">
+      <h1>Work in progress!</h1>
+    </div>
+  );
+}
