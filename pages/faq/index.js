0ea1c2b7ac4ba56b2597478206d42681d43c4456
Tyler Hancock <darklime@live.ca>
2020-09-20 08:12:23 -0600

    Updated page info.

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import Layout from "../../components/Layout";
+import LoginLink from "../../components/LoginLink";
 import PageTitle from "../../components/shared/PageTitle";
 import FAQItem from "./FAQItem";
 
@@ -20,11 +21,20 @@ export default function FAQ() {
       </p>
       <div className="faq">
         <FAQItem title="What is Modtoberfest?">
-          Modtoberfest is a celebration of the open source minecraft modding
+          Modtoberfest is a celebration of the open source Minecraft modding
           community. During the event we challenge participants to create
-          quality pull requests for various community projects. You can sign up
-          any time during October 2020. Modtoberfest is inspired by Digital
-          Ocean's Hactoberfest challenge which has similar goals.
+          quality pull requests for various community projects. You can{" "}
+           <LoginLink>
+             <a className="underline">sign up</a>
+          </LoginLink> any time during October 2020. Modtoberfest is inspired by 
+          Digital Ocean's{" "}
+          <a
+            className="underline"
+            href="https://hacktoberfest.digitalocean.com/"
+            target="_blank"
+          >
+            Hacktoberfest
+          </a> challenge which has similar goals.
         </FAQItem>
         <FAQItem title="What is open source?">
           Open source describes a project that allows anyone to view or modify
@@ -38,6 +48,16 @@ export default function FAQ() {
           project. If the original project approves your changes they can be
           merged and will become a part of the official project.
         </FAQItem>
+        <FAQItem title="What is the Modtoberfest challenge?">
+          We are challenging community members to participate in the open source
+          community. To complete the challenge you must submit a pull request to
+          four different projects that are managed by four different
+          groups/organizations. Pull requests will only be tracked for
+          participating projects. See our{" "}
+           <Link href="/rules">
+             <a className="underline">rules</a>
+          </Link> for more information.
+        </FAQItem>
         <FAQItem title="Is there a prize for participating?">
           Those who complete our challenge will be eligible to receive a sticker
           pack containing many stickers including at least one limited sticker.
@@ -47,6 +67,35 @@ export default function FAQ() {
         <FAQItem title="Does the pull request need to be merged">
           No the pull request does not need to be merged. The only requirements
           are that it passes our quality control policy and is not a draft PR.
+        </FAQItem>
+        <FAQItem title="What time zone does the event use?">
+          We are using{" "}
+          <a
+            className="underline"
+            href="https://www.timeanddate.com/worldclock/fixedtime.html?msg=Modtoberfest&iso=20201001T00&p1=1440"
+            target="_blank"
+          >
+            UTC
+          </a> for the event. All pull request data is taken from
+          GitHub's API which also uses UTC. Please keep in mind that time is
+          tricky and some discrepencies may occur. 
+        </FAQItem>
+        <FAQItem title = "How are pull requests tracked?">
+          We are tracking all pull requests created on participating projects
+          during October 2020. This data is pulled in from the GitHub search
+          API.
+        </FAQItem>
+        <FAQItem title = "Do pull requests made before signing up still count?">
+          Yes we will count any previous pull requests as long as they were made
+          during the event and follow our{" "}
+           <Link href="/rules">
+             <a className="underline">rules</a>
+          </Link>.
+        </FAQItem>
+        <FAQItem title = "Do commits count?">
+          No, we will only be tracking pull requests. Additionally pull
+          requests will only contribute to the challenge score of the 
+          pull request's author. 
         </FAQItem>
         <FAQItem title="How will the rewards be shipped?">
           We will be shipping rewards through Canada Post. International
@@ -62,18 +111,6 @@ export default function FAQ() {
           <br />
           Tracking information will not be provided for rewards and replacements
           are not guaranteed.
-        </FAQItem>
-        <FAQItem title="How can I add my projects to the approved project list?">
-          The ability to add new projects is restricted to those who have
-          directly sponsored the event in some capacity. If you would like to
-          co-sponsor this event, reach out to Darkhax or an organizer in the{" "}
-          <a
-            href="https://discord.modtoberfest.com"
-            target="_blank"
-            className="underline"
-          >
-            Discord server
-          </a>
         </FAQItem>
       </div>
     </Layout>
--- pages/rules/index.js
+++ pages/rules/index.js
@@ -16,22 +16,29 @@ export default function Rules() {
       <div className="rules">
         <h3 className="font-bold tracking-wider mb-2 mt-5">Rules</h3>
         <ol>
-          <li>You must sign up before the end of October 2020</li>
-          <li>Pull requests must be made during October 2020</li>
+          <li>You must sign up before the end of October 2020.</li>
+          <li>Pull requests must be made during October 2020.</li>
           <li>
             Only pull requests against GitHub repositories on our participating
-            projects list count towards your progress in the challenge
+            projects list count towards your progress in the challenge.
           </li>
           <li>
             You must submit a pull request to four{" "}
             <span className="underline">different</span> repositories managed by{" "}
-            <span className="underline">different</span> groups/organizations
+            <span className="underline">different</span> groups/organizations.
           </li>
-          <li>Pull requests must meet our quality standards</li>
+          <li>Pull requests must meet our quality standards.</li>
           <li>
             Rewards and prizes for completing the challenge are limited to one
-            per person
+            per person.
           </li>
+          <br/>
+          <p>
+            In addition to these rules, any attempt to abuse, impede, disrupt,
+            tamper, hack, or meddle in this event will disqualify you from
+            participating.
+          </p>
+          <br/>
           <h3 className="font-bold tracking-wider mb-2 mt-5">
             Quality Control
           </h3>
@@ -40,18 +47,18 @@ export default function Rules() {
             requests will not contribute to your challenge goals. We will be
             manually reviewing all PRs to ensure this policy is enforced.
           </p>
-          <ul>
-            <li>- Pull requests that have been scripted or automated</li>
-            <li>- Pull requests that hurt a project more than they help.</li>
+          <ul className="list-disc">
+            <li>Pull requests that have been scripted or automated.</li>
+            <li>Pull requests that hurt a project more than they help.</li>
             <li>
-              - Pull requests that focus on minor changes such as whitespace,
+              Pull requests that focus on minor changes such as whitespace,
               indenting, typos, and optimizing assets and images.
             </li>
           </ul>
           <p>
             Additionally any project maintainer may mark your pull request with
-            the <code>invalid</code> label which will automatically disqualify
-            that pull request from our systems.
+            the invalid label which will automatically disqualify that pull 
+            request from our systems.
           </p>
         </ol>
       </div>
