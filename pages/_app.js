b86156f4da5895027baa5a734aad5545080dbe04
Antoine Gagnon <antoine.gagnon760@gmail.com>
2020-09-19 20:04:38 -0400

    Add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,36 @@
 import "../css/index.css";
 
 import { Provider } from "next-auth/client";
+import { DefaultSeo } from "next-seo";
+import Head from "next/head";
 
 function App({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
+      <DefaultSeo
+        title="Modtoberfest"
+        description="Celebrating the Minecraft open source community"
+        canonical="https://modtoberfest.com"
+        openGraph={{
+          type: "website",
+          title: "Modtoberfest",
+          url: "https://modtoberfest.com",
+          description: "Celebrating the Minecraft open source community",
+          images: [
+            {
+              url: "https://modtoberfest.com/logo-half.png",
+              alt: "Modtoberfest logo",
+            },
+          ],
+          site_name: "Modtoberfest",
+        }}
+        twitter={{
+          cardType: "summary_large_image",
+        }}
+      />
+      <Head>
+        <link rel="icon" type="image/png" href="/logo-trimmed.png" />
+      </Head>
       <Component {...pageProps} />
     </Provider>
   );
--- pages/index/WIP.js
+++ pages/index/WIP.js
@@ -3,8 +3,7 @@ import React from "react";
 export default function WIP() {
   return (
     <div className="bg-gray-900 text-white flex flex-col items-center min-h-screen justify-center">
-      <h1>Modtoberfest 2020</h1>
-      <p>Work in progress! Stay sturned :)</p>
+      <img src="/logo-trimmed.png" width="500" />
     </div>
   );
 }
