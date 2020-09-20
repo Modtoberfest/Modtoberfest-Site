1c6d6a6a0a04ca3ffff18c4929ecba61e3cae8cd
Antoine Gagnon <antoine.gagnon760@gmail.com>
2020-09-20 03:20:29 -0400

    Fix grid

--- pages/participants/Participant.js
+++ pages/participants/Participant.js
@@ -10,12 +10,12 @@ export default function Participant({
 }) {
   return (
     <div className="w-full lg:w-1/2 xl:w-1/3 p-2 lg:p-5">
-      <div className="h-full p-2 flex flex-wrap md:border-0 border-b-2 border-warm-l-orange">
-        <div className="w-full sm:w-1/2  flex items-center justify-center">
-          <img src={imageUrl || "logo/badge.png"} className="px-5" />
+      <div className="h-full p-2 flex flex-col sm:flex-row flex-grow md:border-0 border-b-2 border-warm-l-orange">
+        <div className="w-full md:w-1/2">
+          <img src={imageUrl || "logo/badge.png"} className="px-5 mb-2" />
         </div>
-        <div>
-          <h2 className="font-bold text-warm-l-orange tracking-wide my-3">
+        <div className="w-full md:w-1/2">
+          <h2 className="font-bold text-warm-l-orange tracking-wide mb-3">
             {name}
           </h2>
           <p>{summary}</p>
