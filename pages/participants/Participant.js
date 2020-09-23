5039ef27dd16a490646b7b52ec8e7187a059903e
Antoine Gagnon <antoine.gagnon760@gmail.com>
2020-09-22 20:21:07 -0400

    Center sponsor images

--- pages/participants/Participant.js
+++ pages/participants/Participant.js
@@ -12,8 +12,8 @@ export default function Participant({
 }) {
   return (
     <div className="w-full lg:w-1/2 xl:w-1/3 p-2 lg:p-5">
-      <div className="h-full p-2 flex flex-col sm:flex-row flex-grow md:border-0 border-b-2 border-warm-l-orange">
-        <div className="w-full md:w-1/2">
+      <div className="h-full p-2 flex flex-col sm:flex-row flex-grow md:border-0 border-b-2 border-warm-l-orange items-center justify-center">
+        <div className="w-full md:w-1/2 flex justify-center">
           <img src={imageUrl || "logo/badge.png"} className="px-5 mb-2" />
         </div>
         <div className="w-full md:w-1/2">
