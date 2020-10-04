c0df4d4df78a4220a2d1cbc023d912314ea7033a
Antoine Gagnon <antoine.gagnon760@gmail.com>
2020-10-04 02:57:25 -0400

    Update notice

--- components/Progress/TrackingNotice.js
+++ components/Progress/TrackingNotice.js
@@ -1,13 +1,22 @@
+import Link from "next/link";
 import React from "react";
 
 export default function TrackingNotice() {
   return (
     <div className="text-center mt-5">
-      <h2>
-        Notice: Your contributions won't show up instantly.
-        <br />
-        Don't worry! They are still accounted for.
-      </h2>
+      <h2 className="font-bold">Notices</h2>
+      <div>Contributions might be delayed for as much as ~10 minutes</div>
+      <div>Only one contribution by repository</div>
+      <div>Invalid pull requests aren't accounted for</div>
+      <p>
+        <Link href="/faq">
+          <a className="font-bold">FAQ</a>
+        </Link>{" "}
+        -{" "}
+        <Link href="/contact">
+          <a className="font-bold">Support</a>
+        </Link>
+      </p>
     </div>
   );
 }
